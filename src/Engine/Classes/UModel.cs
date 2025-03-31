0d660dd5d5ab178c0c507d7b78115b38a111b7a7
Danil6969 <20danil02@mail.ru>
2025-03-31 11:35:13 +0300

    Update UModel.cs

--- src/Engine/Classes/UModel.cs
+++ src/Engine/Classes/UModel.cs
@@ -10,6 +10,22 @@ namespace UELib.Core
     [UnrealRegisterClass]
     public class UModel : UPrimitive
     {
+        public int Int1;
+
+        public long Long1;
+        public long Long2;
+        public long Long3;
+        public long Long4;
+        public long Long5;
+        public long Long6;
+
+        public UObject Object1;
+        public UObject Object2;
+
+        public UVector Vector1;
+
+        public USphere Sphere1;
+
         [Output]
         public UPolys Polys;
         
@@ -20,7 +36,37 @@ namespace UELib.Core
 
         protected override void Deserialize()
         {
-            base.Deserialize();
+            DeserializeNetIndex();
+
+            Long1 = _Buffer.ReadInt64();
+            Record(nameof(Long1), Long1);
+
+            _Buffer.ReadStruct(out Vector1);
+            Record(nameof(Vector1), Vector1);
+
+            _Buffer.ReadStruct(out Sphere1);
+            Record(nameof(Sphere1), Sphere1);
+
+            Long2 = _Buffer.ReadInt64();
+            Record(nameof(Long2), Long2);
+            Long3 = _Buffer.ReadInt64();
+            Record(nameof(Long3), Long3);
+            Long4 = _Buffer.ReadInt64();
+            Record(nameof(Long4), Long4);
+
+            Object1 = _Buffer.ReadObject();
+            Record(nameof(Object1), Object1);
+
+            Int1 = _Buffer.ReadInt32();
+            Record(nameof(Int1), Int1);
+
+            Long5 = _Buffer.ReadInt64();
+            Record(nameof(Long5), Long5);
+            Long6 = _Buffer.ReadInt64();
+            Record(nameof(Long6), Long6);
+
+            Object2 = _Buffer.ReadObject();
+            Record(nameof(Object2), Object2);
         }
     }
 }
